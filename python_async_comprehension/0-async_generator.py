3748a7f091459cc5ddc3d9b42a26f05bfb8b04d1
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-20 18:13:02 +0100

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """then yield function"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
