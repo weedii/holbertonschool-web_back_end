d410dff5df06c9110c771bd3a82cb2a535f5b51c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-20 18:14:44 +0100

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float]:
     """then yield function"""
     for i in range(10):
         await asyncio.sleep(1)
