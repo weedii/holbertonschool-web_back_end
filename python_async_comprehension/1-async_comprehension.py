bfd3c711640c0703d6dabf3a9fa83a23fad1287d
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-20 18:31:15 +0100

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """async_comprehension function"""
-    randomNumbers = []
-    async for i in async_generator():
-        randomNumbers.append(i)
+    randomNumbers = [i async for i in async_generator()]
     return randomNumbers
