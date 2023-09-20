98acdb540c045e796505cb2b01a073a02fd9156f
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-20 10:16:47 +0100

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat function"""
+    return str1+str2
