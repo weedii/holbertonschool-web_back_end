f5722a7a33c83d2904b62f5d54174a11e3e4e075
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-20 17:30:39 +0100

    Tasks

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    return asyncio.Task(wait_random(max_delay))
