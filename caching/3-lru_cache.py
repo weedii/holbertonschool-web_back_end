484ed39f1f620fee7472becbf234da351ac11e72
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-22 22:43:20 +0100

    LRU Caching

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""LRU Caching"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache (BaseCaching):
+    """LRU Caching"""
+
+    def __init__(self):
+        """Init"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        i = 0
+        if key and item:
+            self.cache_data[key] = item
+            if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+                leastKey = next(iter(self.cache_data))
+                print(f"DISCARD: {leastKey}")
+                del self.cache_data[leastKey]
+        else:
+            pass
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
