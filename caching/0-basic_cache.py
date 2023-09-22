f1c8b8bac741add160e8feb7f06a8d0d737e80b0
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-22 16:25:36 +0100

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic dictionary"""
+
+from functools import cache
+from base_caching import BaseCaching
+
+
+@cache
+class BasicCache (BaseCaching):
+    """Basic dictionary"""
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
