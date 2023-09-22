96a761df1dfb892ab880a4a0a45a6405e531d54e
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-22 20:36:46 +0100

    FIFO caching

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -15,10 +15,10 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+        if (len(self.cache_data)-1 >= BaseCaching.MAX_ITEMS+1):
             firstKey = next(iter(self.cache_data))
             print(f"DISCARD: {firstKey}")
-            self.cache_data.pop(firstKey)
+            del self.cache_data[firstKey]
         elif not key or not item:
             pass
         else:
