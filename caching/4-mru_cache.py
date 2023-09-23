e92aa8c0002c7ffac0e209f540568e19e666ada4
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-23 23:52:44 +0100

    MRU Caching

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""MRU Caching"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU Caching"""
+
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            leastKey = next(iter(self.cache_data))
+            print(f"DISCARD: {leastKey}")
+            del self.cache_data[leastKey]
+        if not key or not item:
+            pass
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None
