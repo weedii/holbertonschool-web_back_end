37f8dd5aa0afe10adf30b989496a21bbb53a99af
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-03 16:25:25 +0100

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """filter_datum function"""
     for field in fields:
         message = re.sub(f"{field}=.*?{separator}",
