c402b2f3b56efa9def97885e4871e12714b492f2
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-03 22:40:22 +0100

    Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -35,7 +35,7 @@ def filter_datum(fields: List[str], redaction: str,
     return message
 
 
-PII_FIELDS = ("name", "email", "phone", "password", "ip")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def get_logger() -> logging.Logger:
