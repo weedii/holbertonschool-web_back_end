040cdd05dd02507a6563304f1d6df28038b922c7
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-03 16:24:31 +0100

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """filter_datum function"""
     for field in fields:
         message = re.sub(f"{field}=.*?{separator}",
