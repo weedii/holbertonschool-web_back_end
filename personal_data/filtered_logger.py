7c85326e8a73cfd6be738af45909aa524b80cced
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-03 21:01:11 +0100

    Log formatterLog formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -15,10 +15,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """__init__ method"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """format method to filter values in incoming log records"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
