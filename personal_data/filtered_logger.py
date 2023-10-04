17f818c28d323defc719ff10e2c43af4858a925d
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-04 11:09:00 +0100

    Connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -51,7 +51,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """get_db function that  returns a connector to the database"""
     dbUser = os.getenv("PERSONAL_DATA_DB_USERNAME")
     dbPass = os.getenv("PERSONAL_DATA_DB_PASSWORD")
