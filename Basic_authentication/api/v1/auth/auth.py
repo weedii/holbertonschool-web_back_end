fe44ec25e4fbd9f621b61182b503d6ce0dedc436
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-05 13:42:39 +0100

    Define which routes don't need authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,7 @@ class Auth():
         """require_auth method"""
         if path:
             path = path.rstrip('/')
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0 or path not in excluded_paths:
             return True
         elif path in excluded_paths:
             return False
