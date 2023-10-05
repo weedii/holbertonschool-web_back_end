4a26a3b704fe7fe9a67ce3de50b886b103e3814a
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-05 11:17:12 +0100

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,13 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/unauthorized
+    """
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
