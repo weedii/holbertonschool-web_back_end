fddf72349ddbc7886ddd3b817863f209063c283e
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-06 12:21:21 +0100

    Request validation!

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -44,8 +44,11 @@ def access(error) -> str:
 @app.before_request
 def before_request():
     """before_request function"""
-    auth.require_auth(
-        request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+    exist = auth.require_auth(
+        request.path,
+        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+    if exist == False:
+        abort(200)
     authorization_header = auth.authorization_header(request)
     if authorization_header is None:
         abort(401)
