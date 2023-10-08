83fd883badb75b1ae04161289b9069a5bb639151
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-08 23:25:40 +0100

    Logout

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -42,7 +42,8 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-        if not self.user_id_for_session_id(session_id):
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
             return False
         del self.user_id_by_session_id[session_id]
         return True
