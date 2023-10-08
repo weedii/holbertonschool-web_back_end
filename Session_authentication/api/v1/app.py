e63d01b54f8dbd59b15ed7b46d9bd1352e8e2858
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-08 14:43:12 +0100

    Empty session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    auth = SessionAuth()
 else:
     auth = Auth()
 
