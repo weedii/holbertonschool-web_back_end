4539b2fb1d72058ea3d725ebdb54aeef40b09d4c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-11 11:37:01 +0100

    create user

---
+++ user_authentication_service/db.py
@@ -0,0 +1,41 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    i = 0
+
+    def add_user(self, email, hashed_password):
+        """add_user method that returns a user object"""
+        session_id = self._session
+        self.i += 1
+        user = User(id=self.i, email=email,
+                    hashed_password=hashed_password, session_id=session_id, reset_token="")
+        return user
