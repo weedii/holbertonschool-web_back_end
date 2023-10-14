000dc33fcd4133938937f032ce77823813a37408
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-14 03:19:33 +0100

    User profile

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -52,5 +52,16 @@ def logout():
         abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """profile method"""
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": user.email}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
