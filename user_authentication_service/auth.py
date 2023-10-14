9b9ae193c5eda1753dd8dd633cdc163a6569d0fc
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-14 03:51:37 +0100

    Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -90,3 +90,13 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update_password method that update the user pwd"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd)
+            self._db.update_user(user.id, reset_token=None)
+        except NoResultFound:
+            raise ValueError
