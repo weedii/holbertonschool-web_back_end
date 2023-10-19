0c1287468e2b16ff38acd47207e7fa5aaac91910
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-19 22:35:58 +0100

    Get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def index():
     """index method to render default template"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
