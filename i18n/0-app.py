3f92b4228ce99cee42e10a7da5748132c98d5675
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-19 11:44:15 +0100

    Basic Flask app

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+#!/usr/bin/env python3
+"""Basic Flask app"""
 
-app = Flask(__name__)
-app.route('/')
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/')
+def index():
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
