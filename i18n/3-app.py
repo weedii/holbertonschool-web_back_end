51d3f2064b7b0dd79841776258f0d1b3b17d608b
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-10-19 23:13:31 +0100

    Parametrize templates

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def index():
     """index method to render default template"""
-    return _(render_template("3-index.html"))
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
