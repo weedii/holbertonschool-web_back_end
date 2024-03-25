5ed893e105a4bedbfb6b07ce90c9ca9e3f3e3aea
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-25 07:49:09 +0100

    Email validation to sent

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -7,8 +7,8 @@ AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
-        SET NEW.valid_email = 0
-    END IF
-END; //
+        SET NEW.valid_email = 0;
+    END IF;
+END;
 
 DELIMITER ;
