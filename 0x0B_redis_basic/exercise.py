217e8c0608c6f59b0f93f27cf5151d7e20d828fc
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-25 04:59:09 +0100

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from tkinter import N
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 """
 Cache Class
@@ -17,8 +18,27 @@ class Cache():
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store method generate random key
+        """store method generate random key 
         and store data in it in the redis db"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
+        """will return converted data back to the desired format"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        else:
+            if fn is None:
+                return data
+            else:
+                return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """will automatically parametrize Cache.get"""
+        self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """will automatically parametrize Cache.get"""
+        self.get(key, fn=int)
