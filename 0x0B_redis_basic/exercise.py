006f7b942a9ba4cc3e5107a641764bdeeaa57e32
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-25 04:07:18 +0100

    Writing strings to Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,13 +3,20 @@ import redis
 import uuid
 from typing import Union
 
+"""
+Cach Class
+"""
+
 
 class Cache():
+
     def __init__(self):
+        """__init__ method"""
         self._redis = redis.Redis()
         self._redis.flushdb
 
     def store(self, data):
+        """store method generate random key and store data in it"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
