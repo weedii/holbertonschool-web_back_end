35f277f311594671996db2b6c29927aca5855f95
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-25 07:31:32 +0100

    Email validation to sent

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed
+
+DELIMITER //
+
+CREATE TRIGGER reset_valid_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 1;
+    END IF;
+END//
+
+DELIMITER ;
