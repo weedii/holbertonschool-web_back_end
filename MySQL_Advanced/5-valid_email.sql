5f915b367f8473a569f659b9a48b43db3415bdf8
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-25 07:45:47 +0100

    Email validation to sent

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER reset_valid_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 1;
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
     END IF;
 END; //
 
