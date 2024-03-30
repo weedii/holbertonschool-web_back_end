aba44c15d6e69ce2dacd8d3d85f60d37be142ea6
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-03-30 23:16:34 +0100

    Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ function that changes all topics of a school document based on the name
 def update_topics(mongo_collection, name, topics):
     """update_topics function"""
 
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
