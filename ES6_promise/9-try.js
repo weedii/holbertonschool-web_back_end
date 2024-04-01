721e2572c7d7d2167f47ec9a0500def28d423592
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-01 03:07:02 +0100

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let res;
+  try {
+    res = mathFunction();
+  } catch (error) {
+    res = error.toString();
+  }
+
+  queue.push(res);
+  queue.push("Guardrail was processed");
+  return queue;
+}
