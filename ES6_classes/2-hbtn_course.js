fbd5df4843168effaee51a9b7028f2a599754408
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-02 00:13:12 +0100

    A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,35 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this.checkString(name);
+    this._length = this.checkNumber(length);
+    this._students = this.checkArrayOfStrings(students);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  checkString(str) {
+    if (typeof of(str) === "string") return str;
+    else throw new TypeError(`${str} must be a string`);
+  }
+
+  checkNumber(x) {
+    if (typeof of(x) === "number") return x;
+    else throw new TypeError(`${x} must be a string`);
+  }
+
+  checkArrayOfStrings(arr) {
+    if (Array.isArray(arr) && arr.every((item) => typeof item === "string"))
+      return arr;
+    else throw new TypeError("students must be an array string");
+  }
+}
