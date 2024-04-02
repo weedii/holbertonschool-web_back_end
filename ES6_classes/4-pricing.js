54a681654b23a27b9151bb0e6725dce4aba186a9
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-02 11:53:57 +0100

    Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+}
