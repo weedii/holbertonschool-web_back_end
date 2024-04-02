e532dd8123c7111606ae95aa62a3a2d79b496bb8
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-02 12:35:28 +0100

    A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,17 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
-    }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
   }
 }
