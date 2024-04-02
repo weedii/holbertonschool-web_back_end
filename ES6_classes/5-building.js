73de73d1d403e662b922b8b4e29ac86bf0a2be1b
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-02 13:16:07 +0100

    A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (new.target !== Building && this.evacuationWarningMessage===undefined ) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage',
       );
