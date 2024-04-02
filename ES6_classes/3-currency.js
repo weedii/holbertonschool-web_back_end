f8b8673f4b87bc13d76d09ccff60caa522a010e0
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-02 11:39:31 +0100

    Methods, static methods, computed methods names..... MONEY

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,6 +4,22 @@ export default class Currency {
     this._name = name;
   }
 
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
