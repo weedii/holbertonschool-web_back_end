d43c3aa0a936105bbf970514cd6c52eb2a190efa
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-04 11:57:44 +0100

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.once("data", function (data) {
+  process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write("This important software is now closing\n");
+  process.exit();
+});
