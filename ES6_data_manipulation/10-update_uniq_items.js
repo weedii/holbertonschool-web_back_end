2d1a319276928d6399e45d9741a451d442991d42
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-04 01:37:33 +0100

    More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error("Cannot process");
+    throw new Error('Cannot process');
   }
 
   for (const [key, value] of map) {
