c36c4660d5d475dc2a4fd0ed05a0dc73ab0074fd
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-04 13:27:34 +0100

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   // const name = data.toString().trim();
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write(`Your name is: ${typeof name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
-process.stdin.on("close", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
